5c565c1e2e2b7ec10599a9490e293d8af744f23c
wwevo <evoisat@gmail.com>
2016-09-11 14:49:23 +0200

    *small style-hotfix

--- public/themes/boilerplate/views/activity/scheduled_activities.php
+++ public/themes/boilerplate/views/activity/scheduled_activities.php
@@ -1,6 +1,8 @@
 {##activity_message##}
 {##activity_loop##}
-<a href="{##activity_details_link##}">
-    {##activity_title##} @ {##activity_event_date##} {##activity_event_time##}
-</a>
+<section class="event">
+    <a href="{##activity_details_link##}">
+        {##activity_title##} @ {##activity_event_date##} {##activity_event_time##}
+    </a>
+</section>
 {/##activity_loop##}
