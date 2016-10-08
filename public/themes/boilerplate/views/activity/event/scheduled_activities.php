cf381a1255345b936fd4087b7f0afa3f1087d02f
wwevo <evoisat@gmail.com>
2016-10-08 14:42:39 +0200

    *minor style changes

--- public/themes/boilerplate/views/activity/event/scheduled_activities.php
+++ public/themes/boilerplate/views/activity/event/scheduled_activities.php
@@ -1,8 +1,10 @@
 {##activity_message##}
+<ul>
 {##activity_loop##}
-<section class="event">
+<li class="event">
     <a href="{##activity_details_link##}">
         <span>{##activity_title##}</span> : <time datetime="{##activity_event_date##} {##activity_event_time##}">{##activity_event_date##} @ {##activity_event_time##}</time>
     </a>
-</section>
+</li>
 {/##activity_loop##}
+</ul>
