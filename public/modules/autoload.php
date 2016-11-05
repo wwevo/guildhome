b6c6b38b1a3a3089492b8c3bfa5a4ea40c181ca3
wwevo <evoisat@gmail.com>
2016-11-05 19:14:33 +0100

    * small fix

--- public/modules/autoload.php
+++ public/modules/autoload.php
@@ -1,25 +1,19 @@
 <?php 
-require_once('login/Login.php'); //
-require_once('profile/Profile.php'); //
-require_once('login/Register.php'); //
-require_once('settings/Settings.php'); //
-/*
- * All these require one or all of the modules listed above :
- */
-require_once('identity/Identity.php'); //
-require_once('activity/Activity.php'); //
-require_once('activity/Comment.php'); //
-require_once('activity/Activity_Shout.php'); //
-require_once('activity/event/Activity_Event.php'); //
-require_once('activity/Event/Activity_Event_Roles.php'); //
-require_once('activity/event/Activity_Event_Signups.php'); //
-require_once('activity/event/Activity_Event_Widgets.php'); //
-require_once('activity/Activity_Poll.php'); //
-require_once('activity/Activity_ActionMsg.php'); //
-require_once('pages/Pages.php'); //
-require_once('gw2api/gw2api.php'); //
-require_once('gw2api/gw2api_Widgets.php'); //
-/*
- * for development purposes
- */
-require_once('dev/Dev.php'); //
+require_once('login/Login.php');
+require_once('profile/Profile.php');
+require_once('login/Register.php');
+require_once('settings/Settings.php');
+require_once('identity/Identity.php');
+require_once('activity/Activity.php');
+require_once('activity/Comment.php');
+require_once('activity/Activity_Shout.php');
+require_once('activity/event/Activity_Event.php');
+require_once('activity/event/Activity_Event_Roles.php');
+require_once('activity/event/Activity_Event_Signups.php');
+require_once('activity/event/Activity_Event_Widgets.php');
+require_once('activity/Activity_Poll.php');
+require_once('activity/Activity_ActionMsg.php');
+require_once('pages/Pages.php');
+require_once('gw2api/gw2api.php');
+require_once('gw2api/gw2api_Widgets.php');
+require_once('dev/Dev.php');
