471da6570554f0f1761e2411be27f3c112062a53
wwevo <evoisat@gmail.com>
2016-11-13 18:46:12 +0100

    * integrated ActivityModel into Activity

--- public/modules/activity/Activity.php
+++ public/modules/activity/Activity.php
@@ -1,6 +1,6 @@
 <?php
 
-abstract class Activity extends Pagination {
+abstract class Activity extends Pagination implements IDatabaseModel {
 
     // start model (i suppose)
     protected abstract function getActivityById($activity_id);
@@ -200,5 +200,25 @@ abstract class Activity extends Pagination {
     }
 
     protected abstract function validateActivityTypeDetails();
+
     // end view
+
+    function createDatabaseTables($overwriteIfExists) {
+        $db = db::getInstance();
+        if ($overwriteIfExists) {
+            $sqlDropExistingActivityTables = "DROP TABLE IF EXISTS activities,activity_types";
+            $db->query($sqlDropExistingActivityTables);
+        }
+        $sqlActivityTypesTable = "CREATE TABLE `activity_types` (`id` int(6) unsigned NOT NULL AUTO_INCREMENT,`name` varchar(50) NOT NULL,
+            `description` varchar(100) DEFAULT NULL,`name_plural` varchar(45) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;";
+        $db->query($sqlActivityTypesTable);
+        $sqlInsertActivityTypesTable = "INSERT INTO `activity_types`(`id`,`name`,`description`,`name_plural`) VALUES
+            (1,`shout`,`a shout`,`shouts`),(2,`event`,`an event`,`events`),(3,`poll`,`a poll`,`polls`),(4,`actionmessage`,`an action message`,`actionmessages`)";
+        $db->query($sqlInsertActivityTypesTable);
+        $sqlActivitiesTable = "CREATE TABLE `activities` (`id` int(6) NOT NULL AUTO_INCREMENT,`userid` int(6) NOT NULL,`create_time` int(11) NOT NULL,
+            `type` int(1) DEFAULT NULL,`comments_enabled` tinyint(1) NOT NULL DEFAULT '0',`deleted` int(11) DEFAULT '0',PRIMARY KEY (`id`))
+            ENGINE=InnoDB AUTO_INCREMENT=261 DEFAULT CHARSET=latin1;";
+        $db->query($sqlActivitiesTable);
+    }
+
 }
