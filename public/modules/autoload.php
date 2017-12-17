d2bcdc72291aeb0e346d452331669dbb0983dbb8
evois <evois@ecv-win10-pro.home>
2017-12-17 14:12:12 +0100

    cleanup commit

--- public/modules/autoload.php
+++ public/modules/autoload.php
@@ -5,12 +5,9 @@ require_once "login/Register.php";
 
 require_once "profile/Profile.php";
     require_once "profile/Profile_Settings.php";
-    require_once "profile/Profile_Characters.php";
 
 require_once "settings/Settings.php";
-require_once "identity/Identity.php";
 
 require_once "pages/Pages.php";
 
-require_once "dev/Dev.php";
 require_once "config/Database.php";
--- public/modules/config/Database.php
+++ public/modules/config/Database.php
@@ -1,34 +1,41 @@
-<?php
-
+<?php 
 class Database {
-
+    public $view;
+    
     function initEnv() {
-        Toro::addRoute(["/dbsetup" => 'database']);
+        Toro::addRoute(["/dbsetup" => 'Database']);
     }
-
+    
+    function __construct() {
+        $this->view = new Database_View();
+    }
+    
     function get() {
         $page = Page::getInstance();
         if (!Login::isAdmin()) {
             $page->setContent('{##main##}', "Try again, guy, you are no admin!");
             return false;
         }
-
-        $page->setContent('{##main##}', "");
-        $page->addContent('{##main##}', View::createPrettyButtonForm("/dbsetup", null, "Start DB setup!"));
+        $page->addContent('{##main##}', View::createPrettyButtonForm("/dbsetup", null, "Start DB setup!", $this->view->getSelectTablesFormView()));
     }
-
+    
     function post() {
         if (!Login::isAdmin()) {
+        $page = Page::getInstance();
+            $page->setContent('{##main##}', "Try again, guy, you are no admin!");
             return false;
         }
-//        var_dump($_SESSION['dbconfig']);
-        if (isset($_SESSION['dbconfig'])) {
-            foreach ($_SESSION['dbconfig'] as $model) {
-                $model->createDatabaseTables((boolean) true);
+        $env = Env::getInstance();
+        $selected_models = $env->post('selected_models');
+        if (is_array($_SESSION['dbconfig'])) {
+            foreach ($_SESSION['dbconfig'] as $class_name => $model) {
+                if (false !== $selected_models && is_array($selected_models) && in_array($class_name, $selected_models)) {
+                    $model->createDatabaseTables((boolean) true);
+                }
             }
         }
+        $this->get();
     }
-
 }
 
 $init = new Database();
